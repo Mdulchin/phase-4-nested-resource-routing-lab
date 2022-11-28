e36b61a303da79a7f6083aa96475779631115923
madelinedulchin <madeline.dulchin@gmail.com>
2022-11-28 15:48:13 -0500

    commit tree a6ed83bbbd5b288d05bdbcf8c2ae40df17dd37b8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+def show
+  item = Item.find(params[:id])
+  render json: item
+end
+
+def create
+  user = User.find(params[:user_id])
+  new = user.items.create(item_params)
+  render json: new, status: :created
+end
+
+private
+
+def item_params
+params.permit(:name, :description, :price)
+end
+
+def render_not_found
+  render json: { errors: "user not found" }, status: :not_found
+end
 end
